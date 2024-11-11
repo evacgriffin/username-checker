1ac1fb6ecf719c370b57756e87a28285b7057e09
Eva Griffin <eva@griffin.to>
2024-11-11 13:36:40 -0800

    Add run_test() and refactor

--- test_client.py
+++ test_client.py
@@ -6,6 +6,30 @@ import json
 import zmq
 
 
+def run_test(client_socket, test_user: dict) -> None:
+    """
+    Tests the username checker using the specified client socket and test user dictionary.
+    :param client_socket: Client socket as a zmq socket
+    :param test_user: Test user information as a Python dictionary
+    :return: No return value
+    """
+    action = test_user["action"]
+    # Serialize the dictionary to a JSON string
+    request_string = json.dumps(test_user)
+
+    # Send request to add the new user to the database
+    print(f"Requesting to {action} user...")
+    # Encode message to binary to send
+    binary_msg = request_string.encode('utf-8')
+    client_socket.send(binary_msg)
+
+    # Receive server reply
+    server_reply = client_socket.recv()
+    # Decode the message
+    server_string = server_reply.decode('utf-8')
+    print(f"Received reply: {server_string}")
+
+
 def main():
     # Initialize client socket
     context = zmq.Context()
@@ -13,119 +37,33 @@ def main():
     client_socket = context.socket(zmq.REQ)
     client_socket.connect("tcp://localhost:5555")
 
-    # TEST ADDING A NEW USER
-    # Create dictionary/JSON for a username to be added
-    test_user = {
-        "action": "add",
-        "username": "test_user1"
-    }
-
-    # Serialize the dictionary to a JSON string
-    request_string = json.dumps(test_user)
-
-    # Send request to add the new user to the database
-    print("Requesting to add user to the database...")
-    # Encode message to binary to send
-    binary_msg = request_string.encode('utf-8')
-    client_socket.send(binary_msg)
-
-    # Receive server reply
-    server_reply = client_socket.recv()
-    # Decode the message
-    server_string = server_reply.decode('utf-8')
-    print(f"Received reply: {server_string}")
-
-    # -------------------------------------------------
-    # TEST ADDING ANOTHER USER
-    # Create dictionary/JSON for a username to be added
-    test_user = {
-        "action": "add",
-        "username": "test_user2"
-    }
-
-    # Serialize the dictionary to a JSON string
-    request_string = json.dumps(test_user)
-
-    # Send request to add the new user to the database
-    print("Requesting to add user to the database...")
-    # Encode message to binary to send
-    binary_msg = request_string.encode('utf-8')
-    client_socket.send(binary_msg)
-
-    # Receive server reply
-    server_reply = client_socket.recv()
-    # Decode the message
-    server_string = server_reply.decode('utf-8')
-    print(f"Received reply: {server_string}")
-
-    # -------------------------------------------------
-    # TEST ADDING ANOTHER USER
-    # Create dictionary/JSON for a username to be added
-    test_user = {
-        "action": "add",
-        "username": "test_user3"
-    }
-
-    # Serialize the dictionary to a JSON string
-    request_string = json.dumps(test_user)
-
-    # Send request to add the new user to the database
-    print("Requesting to add user to the database...")
-    # Encode message to binary to send
-    binary_msg = request_string.encode('utf-8')
-    client_socket.send(binary_msg)
-
-    # Receive server reply
-    server_reply = client_socket.recv()
-    # Decode the message
-    server_string = server_reply.decode('utf-8')
-    print(f"Received reply: {server_string}")
+    # TEST ADDING THREE NEW USERS
+    # Create list of dictionary/JSON for usernames to be added
+    test_users = [
+        {"action": "add", "username": "test_user1"},
+        {"action": "add", "username": "test_user2"},
+        {"action": "add", "username": "test_user3"}
+    ]
+    for test_user in test_users:
+        run_test(client_socket, test_user)
 
     # -------------------------------------------------
     # TEST ADDING A DUPLICATE USER
     # Create dictionary/JSON for a username to be added
-    test_user = {
+    duplicate_test_user = {
         "action": "add",
         "username": "test_user2"
     }
-
-    # Serialize the dictionary to a JSON string
-    request_string = json.dumps(test_user)
-
-    # Send request to add the new user to the database
-    print("Requesting to add user to the database...")
-    # Encode message to binary to send
-    binary_msg = request_string.encode('utf-8')
-    client_socket.send(binary_msg)
-
-    # Receive server reply
-    server_reply = client_socket.recv()
-    # Decode the message
-    server_string = server_reply.decode('utf-8')
-    print(f"Received reply: {server_string}")
+    run_test(client_socket, duplicate_test_user)
 
     # -------------------------------------------------
     # TEST DELETING A USER
     # Create dictionary/JSON for a username to be deleted
-    test_user = {
+    delete_test_user = {
         "action": "delete",
         "username": "test_user2"
     }
-
-    # Serialize the dictionary to a JSON string
-    request_string = json.dumps(test_user)
-
-    # Send request to add the new user to the database
-    print("Requesting to delete user from the database...")
-    # Encode message to binary to send
-    binary_msg = request_string.encode('utf-8')
-    client_socket.send(binary_msg)
-
-    # Receive server reply
-    server_reply = client_socket.recv()
-    # Decode the message
-    server_string = server_reply.decode('utf-8')
-    print(f"Received reply: {server_string}")
+    run_test(client_socket, delete_test_user)
 
 
 if __name__ == '__main__':
